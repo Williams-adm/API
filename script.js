a373a53b891c10fee2ab77ebf64b37739bfd05dd
unknown <i2310607@continental.edu.pe>
2024-05-03 22:01:45 -0500

    segundo commit

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const apikey = 'a2398fdfbd317316277fe42c389550e7' /* key publica */
 const ts = '04/05/2024, 02:04:03' /* timestamp -> new Date().toLocaleString('en-GB', {timeZone:'UTC})*/
 const hash = 'a5c11dd0c74a5615241c3ee4eeaef49f' /* union de ts | private | public */
 const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}`
+
+fetch(url)
+    .then(response => response.json())
+    .then(response => console.log(response.data.results))
+    .catch( error => console.log('Se ha producido un error: ', error))
